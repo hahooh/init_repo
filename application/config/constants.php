704ae9e25558d2a37983869e2e0401b0d92e68da
Jiwan Kim <hahooh@gmail.com>
2017-06-25 00:01:07 +0900

    remvoe unnecessary lines

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,10 +83,3 @@ defined('EXIT_USER_INPUT') OR define('EXIT_USER_INPUT', 7); // invalid user inpu
 defined('EXIT_DATABASE') OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN') OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX') OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
-
-/*
- * Let me buy stuff constants
- */
-defined('USER_USER_TYPE') OR define('USER_USER_TYPE', 'user');
-defined('ADMIN_USER_TYPE') OR define('ADMIN_USER_TYPE', 'admin');
-defined('CS_USER_TYPE') OR define('CS_USER_TYPE', 'cs');
